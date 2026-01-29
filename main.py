48818a6dae0aa1b94e18ef2cccead8be3904a263
jorudolph1 <10979879@uvu.edu>
2026-01-28 19:58:32 -0700

    Update main function to work with the new load_words
    Modified main function to accept a file name input for loading words.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from prototype1 import load_words, memory, execute_program
 
 def main():
-    load_words()
+    file_name = input("Enter the name of the file to load: ")
+    load_words(file_name)
     execute_program()
 
 if __name__ == "__main__":
