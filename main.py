82110d1455de3364846a224c99f872a463b6552c
jorudolph1 <10979879@uvu.edu>
2026-02-18 16:46:07 -0700

    Modify main to accept user input for program words
    Refactor main function to take user input for program words instead of command-line arguments.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from prototype1 import UVSimulator    # Import UVUSimlator class
-import sys                            # Import sys to access command-line arguments
 
 
 def main():
     # Get the file name from the command-line arguments
     # sys.argv[1] is the first argument after the script name
-    file_name = sys.argv[1]
+    #file_name = sys.argv[1]
 
     # Create the simulator instance using the input file
-    simulator = UVSimulator(file_name)
-
-    # Load the program words from the file into memory
-    simulator.load_words()
-
+    simulator = UVSimulator()
+    # Take user input for program words and load them into memory
+    simulator.load_from_text(input("Enter your program words (separated by spaces, newlines, or tabs): "))
     # Execute the program stored in memory
     simulator.execute_program()
     
